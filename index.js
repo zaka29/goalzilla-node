f164ca29537632f7a5c91f6f289e1107e505bb2a
zaka29 <zaka29@gmail.com>
2020-02-03 11:47:26 +0200

    Luanches and rockets schemas

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by hamster1 on 1/2/20.
+ */
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+const app = express();
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    }),
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`App started on ${PORT}`));
